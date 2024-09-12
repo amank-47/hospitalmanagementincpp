422777592f1950058dda0d41ce295d0847083851
Harsahibjit Singh <129825567+Harsahibjit-Singh@users.noreply.github.com>
2024-09-12 10:56:02 +0530

    first commit

---
+++ miniproject.cpp
@@ -0,0 +1,40 @@
+//Features of our projects
+//1. Patient Registration System (Array / Linked List)
+//2. Doctor Management (Array / Linked List)
+//3. Appointment Scheduling (Queue)
+//4. Billing System (Stack)
+//5. Bed/Room Allocation (Linked List)
+//6. Emergency Cases (Priority Queue using Array/Linked List)
+//7. Medicine Stock Management (Array)  [optional]
+#include<iostream>
+using namespace std;
+
+//void patient_registration(){
+//	
+//}
+
+int main(){
+
+cout<<"------------------------------------------------------------------------"<<endl;
+cout<<" "<<endl;
+cout<<"                          HOSPITAL MANAGEMENT SYSTEM                     "<<endl;
+cout<<""<<endl;
+cout<<"------------------------------------------------------------------------"<<endl;
+cout<<""<<endl;
+cout<<"Press the respective button for the operation you want to perform"<<endl;
+cout<<"1 for Patient Registration "<<endl;
+cout<<"2 for Doctor's duty Management"<<endl;
+cout<<"3 for Appointment Schedule"<<endl;
+cout<<"4 for Bill Generation"<<endl;
+cout<<"5 for Bed/Room Allocation"<<endl;
+cout<<"6 for Emergency Cases"<<endl;
+cout<<"7 for Medicine Stock Management"<<endl;
+int choice;
+cout<<"Enter your choice: ";
+cin>>choice;
+switch(choice){
+	case '1':
+//		patient_registration();
+cout<<" hello";
+}
+}
